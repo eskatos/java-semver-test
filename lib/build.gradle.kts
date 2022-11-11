aff44c0afca243300000869c0dd05857677f263b
Paul Merlin <paul@nosphere.org>
2022-11-11 13:45:02 +0100

    impl

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    testImplementation(gradleApi())
+    testImplementation("org.apache.maven:maven-artifact:3.8.6")
+    testImplementation("com.github.zafarkhaja:java-semver:0.9.0")
+    testImplementation("com.vdurmont:semver4j:3.1.0")
+    testImplementation("io.github.g00fy2:versioncompare:1.5.0")
 }
 
 testing {
